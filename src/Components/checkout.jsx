41c8263af3a31ac194cad5c1d135272d540b2387
vikas <vikasnwrdr2001@gmail.com>
2022-05-20 06:31:18 +0530

    cahne

--- src/Components/checkout.jsx
+++ src/Components/checkout.jsx
@@ -36,16 +36,16 @@ export const Checkout = () => {
                 <form action="">
 
                     <label style={{color : "white" , marginLeft : "20px"}}> Cardholder Name</label> <br />
-                    <input style={{backgroundColor : "black" , borderColor : "white" , borderRadius : "10px" , width : "80%" ,height:"30px",marginLeft:"20px"}} type="text" name="" id="" /> <br /><br />
+                    <input style={{backgroundColor : "black" , color:"white", borderColor : "white" , borderRadius : "10px" , width : "80%" ,height:"30px",marginLeft:"20px"}} type="text" name="" id="" /> <br /><br />
 
                     <label style={{color : "white" , marginLeft : "20px"}}>Account Number</label> <br />
-                    <input style={{backgroundColor : "black" , borderColor : "white" , borderRadius : "10px" , width : "80%" ,height:"30px",marginLeft:"20px"}} type="text" name="" id="" /><br /><br />
+                    <input style={{backgroundColor : "black" , color:"white", borderColor : "white" , borderRadius : "10px" , width : "80%" ,height:"30px",marginLeft:"20px"}} type="text" name="" id="" /><br /><br />
                   
                     <label style={{color : "white" , marginLeft : "20px"}}> Expiration Date </label> <br />
-                    <input style={{backgroundColor : "black" , borderColor : "white" , borderRadius : "10px" , width : "80%" ,height:"30px",marginLeft:"20px"}} type="text" name="" id="" /><br /><br />
+                    <input style={{backgroundColor : "black" , color:"white", borderColor : "white" , borderRadius : "10px" , width : "80%" ,height:"30px",marginLeft:"20px"}} type="text" name="" id="" /><br /><br />
 
                     <label style={{color : "white" , marginLeft : "20px"}}> CVV No</label> <br />
-                    <input style={{backgroundColor : "black" , borderColor : "white" , borderRadius : "10px" , width : "80%" ,height:"30px",marginLeft:"20px"}} type="text" name="" id="" /><br /><br />
+                    <input style={{backgroundColor : "black" , color:"white", borderColor : "white" , borderRadius : "10px" , width : "80%" ,height:"30px",marginLeft:"20px"}} type="text" name="" id="" /><br /><br />
 
                        <Link to="/home"><button 
                             style={{backgroundColor : "green" , borderColor : "green" ,  color : "white" , borderRadius : "10px" , width : "80%" ,height:"30px",marginLeft:"20px"}}
